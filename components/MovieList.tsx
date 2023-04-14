cea1b57db97372e074176d8c6fd9ec0e159f0858
Aldamsito <alejandrodamasopastor@gmail.com>
2023-04-14 12:24:06 +0200

    cambios en listas

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -14,11 +14,12 @@ function MovieList({ decadeData }: MovieListProps) {
   console.log('MovieList rendered');
 
   const list = useMemo(() => decadeData.reverse().map((decade) => (
-    <div className={styles.card} key={decade.decade}>
+    <div key={decade.decade}>
       <h1>{decade.decade.replace(',', ' - ')}</h1>
       <ul>
         {decade.data.map((movie: Movie, index) => (
           <li key={movie.ID}>
+            <h2 className={movieStyles.movietitle}>{(index + 1).toString().padEnd(2, '\u00A0')} - {movie.Title}, {movie.Year}</h2>
             <div className={movieStyles.movie}>
               <Image
                 src={`https://image.tmdb.org/t/p/original/${movie.Image_path}`}
@@ -33,12 +34,10 @@ function MovieList({ decadeData }: MovieListProps) {
                 blurDataURL="/spinner.svg"
                 alt="image is not available"
               />
-              <div className={movieStyles.movieInfo}>
-                <h2 className={movieStyles.movieTitle}>{(index + 1).toString().padEnd(2, '\u00A0')} - {movie.Title}, {movie.Year}</h2>
-                <p className={movieStyles.movieDetails}> Avg. Ratings: {movie.Rating} </p>
-                <p className={movieStyles.movieDetails}> Vote count:{movie.Votes}</p>
-                <h3>Overview</h3>
-                <p className={movieStyles.movieDetails}> {movie.Overview}</p>
+              <div className={movieStyles.movieinfo}>
+                <p className={movieStyles.moviedetails}> Avg. Ratings: {movie.Rating} </p>
+                <p className={movieStyles.moviedetails}> Vote count:{movie.Votes}</p>
+                <p className={movieStyles.moviedescription}> {movie.Overview}</p>
               </div>
             </div>
           </li>
